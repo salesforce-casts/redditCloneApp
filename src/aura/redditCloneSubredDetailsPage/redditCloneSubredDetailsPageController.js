329622a63da750321dfef8f75c905fe449ff4b2c
Salesforce Casts <teja@salesforcecasts.com>
2018-06-01 23:14:07 +0530

    finished - navigation to a specific subreddit and displying of subreddit details

---
+++ src/aura/redditCloneSubredDetailsPage/redditCloneSubredDetailsPageController.js
@@ -0,0 +1,29 @@
+({
+	doInit : function(component, event, helper) {
+		var subred_id = component.get("v.pageReference.state.rec_id");
+        
+		var action = component.get("c.fetchSubredDetails");
+        
+        
+        action.setParams({
+            rec_id: subred_id
+        });
+        
+        action.setCallback(this, function(response){
+            var state =  response.getState();
+            
+            if(state === "SUCCESS"){
+                //TODO display the record details
+                
+                var subred_details = response.getReturnValue();
+                console.log(subred_details);
+                component.set("v.subRedDetails", subred_details);
+            }else{
+                
+            }
+        });
+        
+        $A.enqueueAction(action);
+        
+	}
+})
--- src/aura/redditCloneSubredditList/redditCloneSubredditListController.js
+++ src/aura/redditCloneSubredditList/redditCloneSubredditListController.js
@@ -18,6 +18,22 @@
     },
     
     goToSubreddit : function (component, event, helper){
+        var navToSubredDetails = component.find('navToSubredDetails');
         
-    }
+        var ctarget = event.currentTarget;
+        var id_str = ctarget.dataset.id;
+        
+        var pageReference = {
+            "type": "standard__component",
+            "attributes": {
+                "componentName": "c__redditCloneSubredDetailsPage"
+            },
+			"state": {
+				"rec_id": id_str //id of the subreddit 
+			}    
+                
+		}
+        
+        navToSubredDetails.navigate(pageReference);
+	}
 })
