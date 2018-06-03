2c97c4a127b7073da9b842727ae906538c61f0b6
Salesforce Casts <teja@salesforcecasts.com>
2018-06-03 15:07:24 +0530

    Added PageHeader to redditCloneSubredDetailsPage.cmp. Upvote and Downvote is commented.

--- src/aura/redditCloneSubredDetailsPage/redditCloneSubredDetailsPageController.js
+++ src/aura/redditCloneSubredDetailsPage/redditCloneSubredDetailsPageController.js
@@ -4,20 +4,19 @@
         
 		var action = component.get("c.fetchSubredDetails");
         
-        
         action.setParams({
             rec_id: subred_id
         });
         
         action.setCallback(this, function(response){
             var state =  response.getState();
-            
             if(state === "SUCCESS"){
                 //TODO display the record details
-                
-                var subred_details = response.getReturnValue();
+                var subred_details = response.getReturnValue();                
+                component.set('v.subRedDetails', subred_details);
+                console.log(subred_details.CreatedDate);
                 console.log(subred_details);
-                component.set("v.subRedDetails", subred_details);
+                component.set('v.subRedPostedDate', new Date(subred_details.CreatedDate));
             }else{
                 
             }
@@ -25,5 +24,21 @@
         
         $A.enqueueAction(action);
         
-	}
+	},
+    
+    upVoteHandler : function(component, event, helper){
+        if(component.get('v.downvote')){
+            console.log('v.downvote');
+            component.set('v.downvote', false);
+        }
+        component.set('v.upvote', !component.get('v.upvote'));
+    },
+    
+    downVoteHandler : function(component, event, helper){
+        if(component.get('v.upvote')){
+            console.log('v.upvote');
+            component.set('v.upvote', false);
+        }
+        component.set('v.downvote', !component.get('v.downvote'));
+    }
 })
