a75ad9c20ff879ec438a5d2bf6fac58287ea6bc2
Salesforce Casts <teja@salesforcecasts.com>
2018-06-12 20:04:51 +0530

    Making Subreddit details page functional using Attribute Syncing

--- src/aura/allPostsOfASubred/allPostsOfASubredController.js
+++ src/aura/allPostsOfASubred/allPostsOfASubredController.js
@@ -21,21 +21,5 @@
         });
         
         $A.enqueueAction(action);
-	},
-    
-    upVoteHandler : function(component, event, helper){
-        if(component.get('v.downvote')){
-            console.log('v.downvote');
-            component.set('v.downvote', false);
-        }
-        component.set('v.upvote', !component.get('v.upvote'));
-    },
-    
-    downVoteHandler : function(component, event, helper){
-        if(component.get('v.upvote')){
-            console.log('v.upvote');
-            component.set('v.upvote', false);
-        }
-        component.set('v.downvote', !component.get('v.downvote'));
-    }
+	}
 })
